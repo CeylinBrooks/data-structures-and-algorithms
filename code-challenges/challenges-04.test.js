0665eb05e1695a5908adbcc4ed08aa20bad3db6d
ceylinbrooks <ceylinbrooks@gmail.com>
2021-02-20 22:22:19 -0500

    Working on Challenges

--- code-challenges/challenges-04.test.js
+++ code-challenges/challenges-04.test.js
@@ -69,7 +69,13 @@ Write a function named containsWorld that takes in a string or number of any len
 ------------------------------------------------------------------------------------------------ */
 
 const containsWorld = (input) => {
-  // Solution code here...
+  const result = /^\d\'world'/.test(input);
+  if (input){
+  return true;
+  } else {
+    return false
+  }
+
 };
 
 /* ------------------------------------------------------------------------------------------------
