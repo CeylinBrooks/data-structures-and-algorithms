e132e9cae68ccf248ee8474f8cb367a0cce62bf9
ceylinbrooks <ceylinbrooks@gmail.com>
2021-03-03 20:57:50 -0500

    CC 10 DONE

--- code-challenges/challenges-10.test.js
+++ code-challenges/challenges-10.test.js
@@ -39,7 +39,13 @@ For example, count(5, [[1, 3, 5, 7, 9], [5, 5, 5], [1, 2, 3]]) returns 4.
 ------------------------------------------------------------------------------------------------ */
 
 const count = (target, input) => {
-  // Solution code here...
+  return input.reduce((accumulator, currentValue) => {
+    const fives = currentValue.filter((value) => {
+      return value === target; 
+    });
+    const numberOfFives = fives.length; 
+    return accumulator += numberOfFives;
+  }, 0);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -53,7 +59,13 @@ For example, [[1, 2, 3, 4, 5], [6, 7, 2, 4, 5, 7], [9, 2, 3, 6,]] returns 66.
 ------------------------------------------------------------------------------------------------ */
 
 const totalSum = (input) => {
-  // Solution code here...
+  return input.reduce((accum, curr) => {
+    accum+= curr.reduce((accum2, curr2) => {
+      accum2 += curr2;
+      return accum2;
+    }, 0);
+    return accum;
+  }, 0);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -69,7 +81,17 @@ For example, [ [0,2,5,4], [2,4,10], [] ] should return [ [1, 32], [1024], [] ].
 ------------------------------------------------------------------------------------------------ */
 
 const divisibleByFiveTwoToThePower = (input) => {
-  // Solution code here...
+  let result = [];
+  input.filter(value =>{
+    result.push(value.filter(newVal => {
+      return newVal % 5 === 0 && typeof(newVal) === 'number';
+    }))
+  })
+  return result.map(value =>{
+    return value.map(newVal =>{
+      return 2**newVal;
+    })
+  })
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -135,7 +157,11 @@ let starWarsData = [{
 }];
 
 let findMaleAndFemale = (data) => {
-  // Solution code here...
+  return data.filter(people => {
+    return people.gender === 'female' || people.gender === 'male';
+  }).map(character => {
+    return character.name;
+  }).join(' and ');
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -145,7 +171,16 @@ Write a function named findShortest that, given the Star Wars data from Challeng
 ------------------------------------------------------------------------------------------------ */
 
 let findShortest = (data) => {
-  // Solution code here...
+  function compare (l, r) {
+    if (l.height < r.height){
+      return 1;
+    } else if (l.height > r.height) {
+      return -1;
+    } else {
+      return 0;
+    }
+  }
+  return data.sort(compare)[0].name;
 };
 
 /* ------------------------------------------------------------------------------------------------
