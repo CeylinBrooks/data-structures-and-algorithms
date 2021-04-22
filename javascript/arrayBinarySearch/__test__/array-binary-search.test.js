cd51c8884b1db19d4e993d4ac4af5ad0dd03d7fd
ceylinbrooks <ceylinbrooks@gmail.com>
2021-04-21 19:54:44 -0500

    Challenge no test

---
+++ javascript/arrayBinarySearch/__test__/array-binary-search.test.js
@@ -0,0 +1,5 @@
+describe('Testing challenge 1', () => {
+  test('It should return an array with 1 added to each value of the original array', () => {
+    expect(addOne([1, 2, 3, 4, 5])).toStrictEqual([2, 3, 4, 5, 6]);
+  });
+});
